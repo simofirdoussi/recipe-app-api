8a7d9310eac42a1e512c05d4e76e141fd6f36a50
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-08-30 11:20:23 +0100

    add tests for db connection

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Django command to wait for the database to be available.
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for db."""
+
+    def handle(self, *args, **options):
+        pass
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,32 @@
+"""
+Test custom Django management commads.
+"""
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """ Test waiting for database if ready. """
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, pathced_check):
+        """ Test witing for database when getting OperationalError. """
+        pathced_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(pathced_check.call_count, 6)
+        pathced_check.assert_called_with(database=['default'])
