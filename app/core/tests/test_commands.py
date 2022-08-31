29fcf0a42e8ff2918c5d86e27d57635ebb136eb7
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-08-31 10:30:52 +0100

    fix: test command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -19,7 +19,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, pathced_check):
@@ -29,4 +29,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(pathced_check.call_count, 6)
-        pathced_check.assert_called_with(database=['default'])
+        pathced_check.assert_called_with(databases=['default'])
