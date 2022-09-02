365d7026d3bb59632a3dbbac00532ed890fb5846
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-09-02 16:43:13 +0100

    add admin page user modification

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,7 +5,7 @@ Django customization.
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.translation import gettext_lazy as _
 from core import models
 
 
@@ -13,6 +13,22 @@ class UserAdmin(BaseUserAdmin):
     """ define the admin pages for users. """
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -29,3 +29,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """ Test edit user page works. """
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
