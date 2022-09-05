1aa402a9b088fe134534d267a3616382b9e58e73
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-09-05 21:12:13 +0100

    docs on hold, error

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
+
+rest_framework = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
