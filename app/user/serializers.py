6e26450dd12944173ebfdbd549d6328cd6b4896a
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-09-07 10:43:34 +0100

    add create token view and serializer

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,6 +2,11 @@
 serializers for the user api view
 """
 
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
@@ -17,3 +22,27 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='Authorization')
+
+        attrs['user'] = user
+        return attrs
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -81,7 +81,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertIn('token', res.data)
-        self.assertEqual(res.status_code, status.HTTP_200_ok)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_bad_creds(self):
         """Test returns error if creds are invalid."""
