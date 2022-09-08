c6508fa7e0da5d9f7c1f8d674dc9d6c019b48900
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-09-08 14:45:32 +0100

    fix: swagger docs

--- app/app/settings.py
+++ app/app/settings.py
@@ -133,6 +133,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
-rest_framework = {
+REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
