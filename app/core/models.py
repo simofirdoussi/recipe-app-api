febf52c65e1abebbd63b58fdd3269dd70ac23a38
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-09-19 11:54:38 +0100

    ingredient retrieve update and delete features done with unit testing

--- app/core/models.py
+++ app/core/models.py
@@ -57,6 +57,7 @@ class Recipe(models.Model):
     description = models.TextField(blank=True)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
