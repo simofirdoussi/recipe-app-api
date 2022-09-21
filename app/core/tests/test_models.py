e4ae40155a70852a3520ac7ef16fe66de235c97b
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-09-21 10:19:01 +0100

    add upload image filename test and feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,8 @@
 """
 Tests for models.
 """
+from unittest.mock import patch
+
 from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -86,3 +88,12 @@ class ModelTests(TestCase):
             name='Ingredient name.'
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating unique image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
